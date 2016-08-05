8c238985ccd170f1968edb45034c0ea1783d297f
Jakub Żuchowski <ellmunzai@gmail.com>
2016-08-05 20:28:31 +0200

    putting DSL into FactoryBoy:: scope
    pretty sure bare DSL might be conflicting with a lot of stuff

--- lib/dsl.rb
+++ lib/dsl.rb
@@ -1,17 +1,19 @@
-class DSL
-  attr_accessor :attribute_hash
+class FactoryBoy
+  class DSL
+    attr_accessor :attribute_hash
 
-  def initialize
-    @attribute_hash = {}
-  end
+    def initialize
+      @attribute_hash = {}
+    end
 
-  def self.run block
-    dsl = new
-    dsl.instance_eval &block
-    dsl
-  end
+    def self.run block
+      dsl = new
+      dsl.instance_eval &block
+      dsl
+    end
 
-  def method_missing method_sym, argument, &block
-    attribute_hash[method_sym] = argument
+    def method_missing method_sym, argument, &block
+      attribute_hash[method_sym] = argument
+    end
   end
 end
